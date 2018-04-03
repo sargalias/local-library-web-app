aa68c1159eec3098e5eae6cb3ca61ceb6974daab
Spyros Argalias <sargalias@gmail.com>
2018-04-03 03:15:50 +0100

    add routes books

---
+++ routes/books.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bookController = require('../controllers/bookinstanceController');
+
+const router = express.Router();
+
+
+// ROUTES
+
+// Index
+router.get('/', bookController.bookinstance_list);
+
+// New
+router.get('/new', bookController.bookinstance_new);
+
+// Create
+router.post('/', bookController.bookinstance_create);
+
+// Show
+router.get('/:author_id', bookController.bookinstance_show);
+
+// Edit
+router.get('/:author_id', bookController.bookinstance_edit);
+
+// Update
+router.update('/:author_id', bookController.bookinstance_update);
+
+// Delete
+router.delete('/:author_id', bookController.bookinstance_delete);
+
+// Delete form
+router.get('/:author_id/delete', bookController.bookinstance_delete_form);
+
+
+
+module.exports = router;
