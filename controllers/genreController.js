7e43561dc794a919fef56fd0ebdac36be819847e
Spyros Argalias <sargalias@gmail.com>
2018-04-04 00:27:29 +0100

    genre detail page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,7 +1,9 @@
 const genreModel = require('../models/genre');
+const bookModel = require('../models/book');
+const async = require('async');
 
 // Genre list
-exports.genre_list = function(req, res) {
+exports.genre_list = function(req, res, next) {
     genreModel.find()
         .sort({name: 1})
         .exec((err, data) => {
@@ -12,8 +14,27 @@ exports.genre_list = function(req, res) {
 };
 
 // Genre show
-exports.genre_show = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
+exports.genre_show = function(req, res, next) {
+    async.parallel({
+        genre: function(callback) {
+            genreModel.findById(req.params.genre_id, callback);
+        },
+        genre_books: function(callback) {
+            bookModel.find({genre: req.params.genre_id}, callback);
+        }
+    },function(err, results) {
+        if (err) return next(err);
+        if (results.genre == null) {
+            const err = new Error('Genre not found');
+            err.status = 404;
+            return next(err);
+        }
+        res.render('genre_detail', {
+            title: 'Genre Detail',
+            genre: results.genre,
+            genre_books: results.genre_books
+        });
+    });
 };
 
 // Genre new
